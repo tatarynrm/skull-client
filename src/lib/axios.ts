347706e3b3dd50206f71786d03776b7cc04ce47e
tatarynrm <tatarynrm@gmail.com>
2025-05-14 20:57:25 +0300

    ewqewqqew

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://localhost:8810',
+  // baseURL: 'http://localhost:8015',
   baseURL: 'https://api.skulldate.site',
   withCredentials: true
 });
